8337f2b7d645458bc5e82a4c69d229f5bcf5d0c5
aayushext <128252053+aayushext@users.noreply.github.com>
2023-08-30 17:56:14 +0000

    Added reset button, added start button, added being able to change speed when paused and retain it when rotation is resumed.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const direction1Select = document.getElementById('direction1');
 const speed2Input = document.getElementById('speed2');
 const direction2Select = document.getElementById('direction2');
 const pauseRotationButton = document.getElementById('pauseRotation');
+const resetRotationButton = document.getElementById('resetRotation');
 
 let isPaused = false;
 let initialSpeed1 = parseFloat(speed1Input.value);
@@ -29,7 +30,9 @@ inputImage2.addEventListener('change', (event) => {
 });
 
 speed1Input.addEventListener('input', () => {
-	updateImageRotation(image1, speed1Input, direction1Select);
+	if (!isPaused) {
+		updateImageRotation(image1, speed1Input, direction1Select);
+	}
 });
 
 direction1Select.addEventListener('change', () => {
@@ -37,7 +40,9 @@ direction1Select.addEventListener('change', () => {
 });
 
 speed2Input.addEventListener('input', () => {
-	updateImageRotation(image2, speed2Input, direction2Select);
+	if (!isPaused) {
+		updateImageRotation(image2, speed2Input, direction2Select);
+	}
 });
 
 direction2Select.addEventListener('change', () => {
@@ -46,6 +51,15 @@ direction2Select.addEventListener('change', () => {
 
 pauseRotationButton.addEventListener('click', () => {
 	isPaused = !isPaused;
+	if (pauseRotationButton.textContent === 'Start') {
+		speed1Input.value = 5;
+		speed2Input.value = 5;
+		pauseRotationButton.textContent = 'Pause Rotation';
+		isPaused = !isPaused;
+		resetAnimation();
+		updateImageRotation(image1, speed1Input, direction1Select);
+		updateImageRotation(image2, speed2Input, direction2Select);
+	}
 	if (isPaused) {
 		pauseRotationButton.textContent = 'Resume Rotation';
 		pauseRotation();
@@ -53,7 +67,18 @@ pauseRotationButton.addEventListener('click', () => {
 		pauseRotationButton.textContent = 'Pause Rotation';
 		resumeRotation();
 	}
+
+	resetAnimation();
 });
+
+resetRotationButton.addEventListener('click', () => {
+	resetAnimation();
+});
+
+function load() {
+	pauseRotation();
+	pauseRotationButton.textContent = 'Start';
+}
 
 function updateImageRotation(image, speedInput, directionSelect) {
 	const speed = parseFloat(speedInput.value);
@@ -67,11 +92,32 @@ function pauseRotation() {
 	initialSpeed2 = parseFloat(speed2Input.value);
 	speed1Input.value = '0';
 	speed2Input.value = '0';
+	updateImageRotation(image1, speed1Input, direction1Select);
+	updateImageRotation(image2, speed2Input, direction2Select);
 }
 
 function resumeRotation() {
-	speed1Input.value = initialSpeed1;
-	speed2Input.value = initialSpeed2;
+	if (parseFloat(speed1Input.value) == 0) speed1Input.value = initialSpeed1; 
+	if (parseFloat(speed2Input.value) == 0)speed2Input.value = initialSpeed2;
 	updateImageRotation(image1, speed1Input, direction1Select);
 	updateImageRotation(image2, speed2Input, direction2Select);
+}
+
+function resetAnimation() {
+	let image1 = document.querySelector("#image1");
+	let image2 = document.querySelector("#image2");
+
+	image1.style.animationName = "none";
+	image2.style.animationName = "none";
+
+	requestAnimationFrame(() => {
+		setTimeout(() => {
+			image1.style.animationName = ""
+		}, 0);
+	});
+	requestAnimationFrame(() => {
+		setTimeout(() => {
+			image2.style.animationName = ""
+		}, 0);
+	});
 }
